ac3352a1f74fb55dc381233c53ba19c7b33ed1f4
Fabio Cortés <fcor14@hotmail.com>
2018-08-22 14:58:47 -0500

    Base files

---
+++ src/components/Stream.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Stream = ({ tracks = [] }) =>
+  tracks.map((track, key) => {
+    return (
+      <div className="track" key={key}>
+        {track.title}
+      </div>
+    )
+  })
+
+
+
+export default Stream
